23da7568d899a74bb026ef5df7f7f5d08d8396da
jianxingren <1925395050@qq.com>
2016-06-15 17:54:34 +0800

    ww

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fml
+//
+//  Created by hgy on 16/6/15.
+//  Copyright © 2016年 hgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fml
+//
+//  Created by hgy on 16/6/15.
+//  Copyright © 2016年 hgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
